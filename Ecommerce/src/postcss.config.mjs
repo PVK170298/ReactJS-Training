edeeec60d07beb2e6356af06c78d3d23f7af0f58
PVK170298 <pandaravelkannan@gmail.com>
2025-05-29 10:31:19 +0000

    Delete Ecommerce/src/postcss.config.mjs

--- Ecommerce/src/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-// Change: module.exports = {
-  export default { // <-- This is the change you need to make
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  };
